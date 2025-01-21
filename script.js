0730eb485498779f3fc33841667148727581480d
hisham763 <hisham.islam2@student.tdsb.on.ca>
2025-01-20 21:08:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,7 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/Ya3h0ugsc/
-";
+let URL = "https://teachablemachine.withgoogle.com/models/bpPb5ikpI/";
 
 
 
@@ -119,27 +118,35 @@ function checkPose(prediction, video) {
 
         switch(poseNumber) {
             case '1':
-                if (time >= 3.5 && time <= 5.5 && !poseState.triggered) {
+                if (time >= 0.9 && time <= 3.0 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
             case '2':
-                if (time >= 10.0 && time <= 12.0 && !poseState.triggered) {
+                if (time >= 5.5 && time <= 7.5 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
             case '3':
-              if (time >= 13.0 && time <= 15.0 && !poseState.triggered) {
-                    triggerExplosion(poseState);
+                if ((time >= 11.5 && time <= 13.0 && !poseState.firstWindowTriggered) ||
+                    (time >= 17.5 && time <= 19.5 && !poseState.secondWindowTriggered)) {
+                    if (time <= 13.0) {
+                        poseState.firstWindowTriggered = true;
+                    } else {
+                        poseState.secondWindowTriggered = true;
+                    }
+                    explosionActive = true;
+                    playExplosionSound();
+                    setTimeout(() => { explosionActive = false; }, 300);
                 }
                 break;
             case '4':
-                if (time >= 16.0 && time <= 18.0 && !poseState.triggered) {
+                if (time >= 15.5 && time <= 16.6 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
             case '5':
-                if (time >= 22.0 && time <= 24.0 && !poseState.triggered) {
+                if (time >= 19.5 && !poseState.triggered) {
                     triggerExplosion(poseState);
                 }
                 break;
@@ -224,7 +231,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/Ya3h0ugsc/");
+        console.log("https://teachablemachine.withgoogle.com/models/bpPb5ikpI/");
     }
 }
 
